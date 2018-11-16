1db22f94fe18786954f72d0588bcaafd8ed80d00
Victor Cazacov <cazacov@gmail.com>
2018-11-16 22:53:23 +0100

    Use simple yaw controller to steer the vehicle

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,14 +1,34 @@
+import rospy
+from yaw_controller import YawController
+
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
 
 
 class Controller(object):
-    def __init__(self, *args, **kwargs):
-        # TODO: Implement
-        pass
+    def __init__(self, vehicle_mass, fuel_capacity, brake_deadband, decel_limit,
+        accel_limit, wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
 
-    def control(self, *args, **kwargs):
-        # TODO: Change the arg, kwarg list to suit your needs
+        # TODO: Implement
+
+        self.vehicle_mass = vehicle_mass
+        self.fuel_capacity = fuel_capacity
+        self.brake_deadband = brake_deadband
+        self.decel_limit = decel_limit
+        self.accel_limit = accel_limit
+        self.wheel_radius = wheel_base
+        self.wheel_base = wheel_base
+        self.steer_ratio = steer_ratio
+        self.max_lat_accel = max_lat_accel
+        self.max_steer_angle = max_steer_angle
+
+        self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
+
+
+    def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
+
+        steering = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
+
         # Return throttle, brake, steer
-        return 1., 0., 0.
+        return 0.5, 0., steering
